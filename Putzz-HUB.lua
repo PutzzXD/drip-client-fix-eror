071dbc944ee02b49654a813afeaca81e51a68f45
Putzzdeveloper <putzzdev@gmail.com>
2026-05-14 20:32:23 +0700

    Add files via upload

--- Putzz-HUB.lua
+++ Putzz-HUB.lua
@@ -5,7 +5,7 @@
 
 -- ================== KEY SYSTEM CONFIG ==================
 local FIREBASE_URL = "https://key-database-701af-default-rtdb.asia-southeast1.firebasedatabase.app/keys.json"
-local WEBSITE_URL = "https://putzzxd.github.io/Drip-Client-GET-KEY.-/"
+local WEBSITE_URL = "https://drip-client-get-key.vercel.app/"
 local SCRIPT_NAME = "DRIP CLIENT"
 
 local SAVE_FILE = "drip_key_data.txt"
